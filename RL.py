e38109ebbd23ff39b92160359047e9f47b66a32b
Filipe Marques <filipe.s.o.marques@gmail.com>
2018-12-05 17:40:33 +0000

    finished RL

--- RL.py
+++ RL.py
@@ -15,7 +15,7 @@ class finiteMDP:
         self.P = P
         self.R = R
         self.absorv = absorv
-        self.alpha = 0.5
+        self.alpha = 0.1
 
 
     def runPolicy(self, n, x0,  poltype = 'greedy', polpar=[]):
@@ -45,7 +45,7 @@ class finiteMDP:
                 nQ[:,a] = self.R[:,a] + self.gamma * np.dot(self.P[:,a,:],self.V)
             err = np.linalg.norm(self.Q-nQ)
             self.Q = np.copy(nQ)
-            if err<1e-7:
+            if err<1e-5:
                 break
 
         #update policy
@@ -58,18 +58,23 @@ class finiteMDP:
 
     def traces2Q(self, trace):
         Q, gamma, alpha = self.Q, self.gamma, self.alpha
-        for l in trace:
-            s1, a1, sf, r  = int(l[0]), int(l[1]), int(l[2]), int(l[3])
-            # alpha = 1? idk
-            Q[s1, a1] = Q[s1, a1] + alpha*(r + gamma*max(Q[sf]) - Q[s1, a1])
+        Q = np.zeros((self.nS, self.nA))
+        nQ = np.zeros((self.nS, self.nA))
+        while True:
+            for l in trace:
+                s1,a1,sf,r=int(l[0]),int(l[1]),int(l[2]),int(l[3])
+                nQ[s1, a1] += alpha*(r+gamma*max(nQ[sf])-nQ[s1, a1])
+            err = np.linalg.norm(Q-nQ)
+            Q = np.copy(nQ)
+            if err<1e-5:
+                break
         return Q
 
     def policy(self, x, poltype = 'exploration', par = []):
-        Q = self.Q
         if poltype == 'exploitation':
-            a = np.argmax(Q[x, :])
+            a = np.argmax(par[x, :])
         elif poltype == 'exploration':
-            a = random.randint(0, len(Q[x])-1)
+            a = random.randint(0, self.nA-1)
         return a
 
     def Q2pol(self, Q, eta=5):
